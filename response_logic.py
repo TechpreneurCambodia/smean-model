8bc4b479f014c707b9e169eaa5723ae46e648499
UnSreypich <unsreypich999@gmail.com>
2025-02-17 14:02:11 +0700

    Refactor response_logic.py to load API key from environment variables

--- response_logic.py
+++ response_logic.py
@@ -7,7 +7,9 @@ safety_settings = {
 import os
 import google.generativeai as genai
 
+from dotenv import load_dotenv
 
+load_dotenv() 
 # Create the model
 generation_config = {
   "temperature": 1,
@@ -16,22 +18,19 @@ generation_config = {
   "max_output_tokens": 8192,
   "response_mime_type": "text/plain",
 }
-genai.configure(api_key="API_KEY")
+genai.configure(api_key=os.getenv("GENAI_API_KEY"))
 
 model = genai.GenerativeModel(
   model_name="gemini-2.0-flash-exp",
   generation_config=generation_config,
 )
 
-
-
-
 def prompt_correction(prompt):
 
   model = genai.GenerativeModel(
     model_name="gemini-2.0-flash-exp",
     generation_config=generation_config,
-    system_instruction="for every input i give to you, return the corrected form of spelling for any typo and misspelling while also correcting potential grammatical error. the input may be english or khmer or a mix of the two but do the same thing for all of the scenario.",
+    system_instruction="for every input i give to you, return the corrected form of spelling for any typo and misspelling while also correcting potential grammatical error. the input may be english or khmer or a mix of the two but do the same thing for all of the scenario. you shall only respond with the corrected transcription in khmer without explaining a single thing about whether you have enough context or not. just respond with the correction.",
   )
   response = model.generate_content(prompt)
   return response.text
